722c1e95fca7175817b0a2b4ce49eb6ffa76987f
Chris Lamb <chris@playfire.com>
2011-03-22 12:00:13 +0000

    Add setup.py file

---
+++ setup.py
@@ -0,0 +1,16 @@
+#!/usr/bin/env python
+
+from setuptools import setup, find_packages
+
+setup(
+    name='django-email-from-template',
+    description="Send emails generated entirely from Django templates.",
+    version='0.1',
+    url='http://code.playfire.com/',
+
+    author='Playfire.com',
+    author_email='tech@playfire.com',
+    license='BSD',
+
+    packages=find_packages(),
+)
